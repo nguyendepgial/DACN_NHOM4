a6762432702d40118be5e2a385342ff52bac17e1
Nguyen <thanhnguyenle123dmx@gmail.com>
2024-12-18 20:36:38 +0700

    Thông điệp commit của anh

--- backend/db_connect.php
+++ backend/db_connect.php
@@ -1,31 +1,25 @@
 <<<<<<< HEAD
 <?php
-// Cấu hình kết nối cơ sở dữ liệu
-$servername = "localhost";  // Địa chỉ máy chủ MySQL
-$username = "root";         // Tên người dùng (mặc định của XAMPP là 'root')
-$password = "";             // Mật khẩu (mặc định của XAMPP là trống)
-$dbname = "moc_nguyen";     // Tên cơ sở dữ liệu
+$servername = "localhost"; 
+$username = "root";         
+$password = "c7fb782c57468694";            
+$dbname = "moc_nguyen";     
 
-// Tạo kết nối
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Kiểm tra kết nối
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
 ?>
 =======
 <?php
-// Cấu hình kết nối cơ sở dữ liệu
-$servername = "localhost";  // Địa chỉ máy chủ MySQL
-$username = "root";         // Tên người dùng (mặc định của XAMPP là 'root')
-$password = "";             // Mật khẩu (mặc định của XAMPP là trống)
-$dbname = "moc_nguyen";     // Tên cơ sở dữ liệu
+$servername = "localhost";  
+$username = "root";         
+$password = "c7fb782c57468694";            
+$dbname = "moc_nguyen";   
 
-// Tạo kết nối
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Kiểm tra kết nối
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
